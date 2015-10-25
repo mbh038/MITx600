a4a0704ea233603a2ba91a2f747791a13d00c65e
mbh038 <mike038@btinternet.com>
2015-10-25 12:16:48 +0000

    Exams

--- 600.1x/Problems/Exam.py
+++ 600.1x/Problems/Exam.py
@@ -19,7 +19,6 @@ d = {'a': 'b'}
 def f(x):
     return 3
 
-a=[9,8,7,6,5,4,3,2,1,0]
 
 def sort1(lst):
     swapFlag = True
@@ -97,7 +96,7 @@ def sort3(lst,n):
             print iteration
             print L
 
-def sort4(lst):
+def sort4(lst,count=0):
     def unite(l1, l2):
         if len(l1) == 0:
             return l2
@@ -111,10 +110,47 @@ def sort4(lst):
     if len(lst) == 0 or len(lst) == 1:
         return lst
     else:
-        front = sort4(lst[:len(lst)/2])
-        back = sort4(lst[len(lst)/2:])
-
+        count +=1
+        front = sort4(lst[:len(lst)/2],count)
+        back = sort4(lst[len(lst)/2:],count)
+        print count
         L = lst[:]  # the next 3 questions assume this line just executed
-        return unite(front, back)
-    print "Count: "+str(count)
+        print front,back
+        print L
+        a = unite(front, back)
+       
+        return a
+
+
+def unite(l1, l2):
+    if len(l1) == 0:
+        return l2
+    elif len(l2) == 0:
+        return l1
+    elif l1[0] < l2[0]:
+        return [l1[0]] + unite(l1[1:], l2)
+    else:
+        return [l2[0]] + unite(l1, l2[1:])
+
+def getSublists(L,n):
+    out=[]
+    for i in range(len(L)-n+1):
+        out.append(L[i:i+n])
     return out
+
+L1 = [10, 4, 6, 8, 3, 4, 5, 7, 7, 2]
+L2= [1, 1, 1, 1, 4]
+
+def longestRun(L):
+    run=[]
+    for i in range (len(L)):
+        run.append(False)
+        subLists=getSublists(L,i+1)
+        for j in range(len(subLists)):
+            if sorted(subLists[j])==subLists[j]:
+                run.pop()
+                run.append(True)
+        if run[i]==False:
+            run.pop()
+            break
+    return len(run)
