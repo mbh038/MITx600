d00b23a4e717389a9715a9dabb5d032bed7795de
mbh038 <mike038@btinternet.com>
2015-10-29 07:58:31 +0000

    random walk

--- 600.2x/FingerExercises/L2 Simulation and random walks.py
+++ 600.2x/FingerExercises/L2 Simulation and random walks.py
@@ -22,3 +22,20 @@ def deterministicNumber():
     # Your code here
     random.seed(123)
     return random.randrange(10,22,2)
+    
+    
+def stochasticNumber():
+    '''
+    Stochastically generates and returns a uniformly distributed even number between 9 and 21
+    '''
+    # Your code here
+    return random.randrange(10,22,2)
+    
+def dist1():
+    return random.random() * 2 - 1
+
+def dist2():
+    if random.random() > 0.5:
+        return random.random()
+    else:
+        return random.random() - 1 
